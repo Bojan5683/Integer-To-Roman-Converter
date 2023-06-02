ccdd61cd14b160f9ed8e9fe426d068e11ebb062a
Bojan <bojan5683@gmail.com>
2023-06-02 17:50:57 +0200

    Adding convert button and some styles

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 const romanSymbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-function intToRoman(num){
-
+const container = document.getElementById('container');
+const convertButton = document.getElementById('convert');
+const input = document.getElementById('input');
 let roman = '';
 
+intToRoman = (num) => {
+
 if(num < 1 || num > 3999){
-    alert('Wrong number!')
+    return "";
 }
-else{
-    for(let i = 0; i < numbers.length; i++){
-        if(num >= numbers[i]){
-            num = num - numbers[i];
-            roman = roman + romanSymbols[i];
-            i--;
-            console.log(roman);
-        }
-    }
-}
-}
-intToRoman(1);
+    else{
+        for(let i = 0; i < numbers.length; i++){
+            while(num >= numbers[i]){
+                num = num - numbers[i];
+                roman = roman + romanSymbols[i];
+                i--;
+        };
+    };
+};
+return roman;
+};
+
+convertButton.addEventListener('click', () => {
+    let number = input.value;
+    intToRoman(number);
+    container.value = roman;
+});
